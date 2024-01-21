e3ab8f193a43dba7a27e2a527153e5f033b529b0
Dayonel <dayonel@hotmail.es>
2024-01-21 20:01:52 +0100

    Add project

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'me.neverminder.app',
+  appName: 'Neverminder',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;

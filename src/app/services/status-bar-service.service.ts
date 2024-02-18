fc1109c3f2d4b4bae9752606580aaba23e82f840
Dayo <dayonel@hotmail.es>
2024-02-18 16:30:23 +0100

    Better way to fix conflict splash screen + status bar.

--- src/app/services/status-bar-service.service.ts
+++ src/app/services/status-bar-service.service.ts
@@ -1,24 +1,17 @@
 import { Injectable } from '@angular/core';
 import { Capacitor } from '@capacitor/core';
-import { SplashScreen } from '@capacitor/splash-screen';
 import { StatusBar, Style } from '@capacitor/status-bar';
-import { Platform } from "@ionic/angular";
 
 @Injectable({
   providedIn: 'root'
 })
 export class StatusBarServiceService {
 
-  constructor(private platform: Platform) { }
-
   async initialize(): Promise<void> {
     if (Capacitor.isNativePlatform()) {
 
-      await SplashScreen.hide(); // bug in Ionic, conflict splash screen + overlay
-      setTimeout(async () => {
-        await StatusBar.setOverlaysWebView({ overlay: true });
-        await StatusBar.setStyle({ style: Style.Light });
-      }, 500);
+      await StatusBar.setOverlaysWebView({ overlay: true });
+      await StatusBar.setStyle({ style: Style.Light });
     }
   }
 }

00bf50e5fe7647257c50fff8ab08c0e79c5d5024
Dayo <dayonel@hotmail.es>
2024-02-20 22:53:59 +0100

    Develop details page.

--- src/app/pages/reminder-details/reminder-details.page.ts
+++ src/app/pages/reminder-details/reminder-details.page.ts
@@ -1,19 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonBackButton, IonButton, IonButtons, IonContent, IonDatetime, IonFooter, IonHeader, IonInput, IonItem, IonLabel, IonList, IonListHeader, IonText, IonTextarea, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonActionSheet, IonBackButton, IonBreadcrumb, IonBreadcrumbs, IonButton, IonButtons, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonContent, IonDatetime, IonDatetimeButton, IonFooter, IonHeader, IonIcon, IonInput, IonItem, IonLabel, IonList, IonListHeader, IonModal, IonNote, IonRange, IonText, IonTextarea, IonTitle, IonToggle, IonToolbar } from '@ionic/angular/standalone';
+import { format } from 'date-fns';
 
 @Component({
   selector: 'app-reminder-details',
   templateUrl: './reminder-details.page.html',
   styleUrls: ['./reminder-details.page.scss'],
   standalone: true,
-  imports: [IonLabel, IonItem, IonList, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonBackButton, IonButtons, IonButton, IonText, IonInput, IonTextarea, IonFooter, IonListHeader, IonDatetime]
+  imports: [IonActionSheet, IonNote, IonToggle, IonIcon, IonRange, IonBreadcrumb, IonBreadcrumbs, IonCard, IonCardContent, IonCardSubtitle, IonCardTitle, IonCardHeader, IonModal, IonDatetimeButton, IonLabel, IonItem, IonList, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonBackButton, IonButtons, IonButton, IonText, IonInput, IonTextarea, IonFooter, IonListHeader, IonDatetime]
 })
 export class ReminderDetailsPage {
+  datetime = format(new Date(), 'dd-MM-yyyy') + 'T09:00:00.000Z';
+  formattedString = '';
+  actionSheetOpen = false;
 
   customCounterFormatter(inputLength: number, maxLength: number) {
     return `${maxLength - inputLength} characters remaining`;
   }
 
+  dateChanged(value: any) {
+    console.log(value);
+    this.datetime = value;
+  }
+
+  public actionSheetButtons = [
+    {
+      text: 'Delete',
+      role: 'destructive',
+      data: {
+        action: 'delete',
+      },
+      handler: () => alert('deleted!')
+    },
+    {
+      text: 'Cancel',
+      role: 'cancel',
+      data: {
+        action: 'cancel',
+      },
+      handler: () => this.actionSheetOpen = false
+    },
+  ];
 }
